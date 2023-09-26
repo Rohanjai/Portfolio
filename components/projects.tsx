d9e9efc3ed5a8e0bb31df1ee18ba83f87d56b107
Rohanjai <jairohan2912@gmail.com>
2023-09-26 17:55:02 +0530

    Fixed about and projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React, { useRef } from "react";
+import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
 import Project from "./project";
 
 export default function Projects() {
-  const ref = useRef(null);
+  const { ref } = useSectionInView("Projects", 0.5);
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
